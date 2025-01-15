5bd9e018fab86e91d0fbc9d1623f3af215c65c81
Pratham vichare <prathamvichare645@gmail.com>
2025-01-15 18:13:50 -0500

    data_Validation done

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 
 ### ETL 
-### Source(DATASET (API,S3 bucket, SOURCES))(EXTRACT)-------------->Transformation(BASIC PREPROCESSING, CLEANING RAW DATA, JSON)------------------------> Destination(MongoDB-> ATLAS, MYSQL, AWS DYNAMODB)(LOAD)
+## Source(DATASET (API,S3 bucket, SOURCES))(EXTRACT)-------------->Transformation(BASIC PREPROCESSING, CLEANING RAW DATA, JSON)------------------------> Destination(MongoDB-> ATLAS, MYSQL, AWS DYNAMODB)(LOAD)
+
+## Data validation -> check the schema if we have have 10 features then its important to train of those feature if 1 of a feature is different then the model will not be trained perfectly  
+if data is change then its a data drift
+## 1.) Same Schema
+## 2.) Data Drift
+## 3.) Validate no. of column, Numerical column exist or not
+
+
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 from networksecurity.logging.logger import logging
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 
 if __name__=='__main__':
     try:
@@ -14,6 +15,14 @@ if __name__=='__main__':
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         logging.info("Data Initiation Completed")
         print(dataingestionartifact)
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
+        data_validation = DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("Initiate_data_validation")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("data validation completed")
+        print(data_validation_artifact)
+
+
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
